065a24b51c0476240d655ba9f0392fda29eeea46
hirnt4e1 <hirnt4e1@protonmail.ch>
2025-04-29 15:58:59 +0700

    Updated main.py, now capped at 60 FPS

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
            f"\nScreen width: {SCREEN_WIDTH}"
            f"\nScreen height: {SCREEN_HEIGHT}"))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
