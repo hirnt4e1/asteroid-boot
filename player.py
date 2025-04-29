b6812f0ac89b832b3a8ddc5a3f9005bac31863d8
hirnt4e1 <hirnt4e1@protonmail.ch>
2025-04-29 16:28:51 +0700

    Updated constants.py, player.py, and main.py. Now the player can be rotated.

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 import circleshape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
@@ -15,5 +15,16 @@ class Player(circleshape.CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
