9e6520111a912ce37209ca266a0554d20d3c2994
hirnt4e1 <hirnt4e1@protonmail.ch>
2025-04-29 17:45:33 +0700

    Added asteroid.py, asteroidfield.py, and updated main.py. Now has asteroids floating around.

---
+++ asteroid.py
@@ -0,0 +1,14 @@
+import pygame
+import circleshape
+
+class Asteroid(circleshape.CircleShape):
+    containers = None
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
