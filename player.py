5b51443c40450e34cb2d6dfa24cc296b7fe5ecaa
hirnt4e1 <hirnt4e1@protonmail.ch>
2025-04-29 16:51:06 +0700

    Updated player.py and main.py. Update and draw functions now handled through groups (and containers)

--- player.py
+++ player.py
@@ -3,6 +3,8 @@ import circleshape
 from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(circleshape.CircleShape):
+    containers = None
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
